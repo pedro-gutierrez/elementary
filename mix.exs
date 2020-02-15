d7484376882d6d5c78df5b9b5810746dceabfab7
Pedro Gutiérrez <pedro.gutierrez@erlang-solutions.com>
2020-02-15 12:49:03 +0100

    upgrade to elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elementary.MixProject do
     [
       app: :elementary,
       version: "0.1.0",
-      elixir: "~> 1.9.1",
+      elixir: "~> 1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
