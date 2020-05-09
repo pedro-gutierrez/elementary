b1f90e4fa7ed4c91f5ec73bb4a099bdc7bdc9c5a
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-05-09 22:11:33 +0200

    router and error page

--- examples/codemutiny/assets/js/elementary-router.js
+++ examples/codemutiny/assets/js/elementary-router.js
@@ -1,26 +1,43 @@
-export default (name, settings, app) => {
-    const {encode, decode, update, tc} = app;
+export default (name, _settings, app) => {
+
+    const {encode, update} = app;
     
     function error(spec, data, reason) {
-        console.error(`[$name]`, {spec, data, reason});
+        console.error(`[${name}]`, {spec, data, reason});
     }
 
     function route() {
-        const hashParts = window.location.hash.split('/');
-        hashParts.shift();
+        const uri = decodeURI(window.location.hash.substring(1))
+
+        var path = "/"
+        var query = {}
+        if (uri.length) {
+            var parts = uri.split("?")
+            path = parts[0]
+            if (parts.length > 1) {
+                parts[1].split('&').map(hk => {
+                    let temp = hk.split('=');
+                    query[temp[0]] = temp[1]
+                });
+            }
+            
+            if (!path.startsWith("/")) path="/"+path;
+        }
+
+
         update({ 
             effect: name,
-            route: hashParts
+            path: path,
+            query: query
         });
     }
 
-    return (encoders, req, model) => {
-        const {err, value} = encode(req, model);
-        if (err) return error(req, model, err);
+    return (_encoders, data, model) => {
+        if (!data) return route();
+        const {err, value} = encode(data, model);
+        if (err) return error(spec, data, err);
         const {action, target} = value;
-        switch(action) {
-            case 'get':
-                return route();
+        switch (action) {
             case 'navigate':
                 window.location.hash = '#/' + target;
                 return route();
